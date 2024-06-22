260de24fcefbcf7ada17ee4c0b3bd9a77e08dfd6
Marcelo Alexandre Barrionuevo <mabarril@gmail.com>
2024-06-22 15:31:42 -0300

    chore: Update autorizacao component HTML template
    Adjust width, height, and font size in autorizacao.component.html to improve layout and styling

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -44,13 +44,12 @@ export class FormularioComponent {
     cpfDbv: [null, Validators.required],
   });
 
-
-
-  sendForm() {
-    let dados = this.addressForm.value;
-    console.log(dados)  }
-
-  onSubmit(): void {
-    alert('Thanks!');
+  dataHoje() {
+    let data = new Date();
+    let dia = data.getDate();
+    let mes = (data.getMonth() + 1).toString().padStart(2, '0');
+    let ano = data.getFullYear();
+    return `${dia}/${mes}/${ano}`;
   }
+
 }
